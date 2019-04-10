36a3ec55cf0e4918fa548add2cb5677fa3862546
22voisbo <49483178+22voisbo@users.noreply.github.com>
2019-04-10 12:40:39 -0400

    adding hello-world.py\

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+# Mr.Grimsley
+#04/10/2019
+####################
+#   Hello World    #
+# our first python #
+#   aplication`    #
+####################
+
+#print "hello, world" to the screen.
+print ("hello, world")
