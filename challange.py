1844e05e0bf8900eb53312fbe8a8717b32e56883
22voisbo <49483178+22voisbo@users.noreply.github.com>
2019-05-29 13:22:36 -0400

    Add files via upload

---
+++ challange.py
@@ -0,0 +1,30 @@
+def main():
+    #Get's info on the user's name than greets them
+    first_name = input("What is your first name?: ")
+    last_name = input("What is your last name?: ")
+    print("Hello, %s %s!"%(first_name,last_name))
+    print()
+
+    #Lists the foods avaliable
+    food = ['Cookie', 'Steak', 'Ice cream', 'Apples']
+    for food in food:
+        print (food)
+
+    #showing age in days, weeks and months
+    print()
+    age_in_years = float(input("How old are you?: "))
+    days_in_years = age_in_years * 365
+    weeks_in_years = age_in_years * 52.143
+    months_in_years = age_in_years * 12
+    print("You're at least",days_in_years,"days old.")
+    print("You,re at least",weeks_in_years,"weeks old.")
+    print("You're at least",months_in_years,"months old.")
+
+    #Makes a humorous statement
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter a adjective: ")
+    place = input("Enter a place: ")
+    print("Take your %s %s and %s it at the %s"%(adjective, noun, verb, place))
+
+main()
